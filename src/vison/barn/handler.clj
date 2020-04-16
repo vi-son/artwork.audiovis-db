f8be95d9680b86f7738a2e9e1e60febfadffd857
Guido Schmidt <git@guidoschmidt.cc>
2020-04-16 23:08:25 +0200

    FIX: namespace renaming to vison.barn, fix routing

---
+++ src/vison/barn/handler.clj
@@ -0,0 +1,70 @@
+(ns vison.barn.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [monger.core :as mg]
+            [monger.collection :as mc]
+            [monger.credentials :as mcr]
+            [cheshire.core :as cc]
+            [taoensso.timbre :as timbre
+             :refer [log  trace  debug  info  warn  error  fatal  report
+                     logf tracef debugf infof warnf errorf fatalf reportf
+                     spy get-env]]
+            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]])
+  (:import [com.mongodb MongoOptions ServerAddress]))
+
+(defn db-connection []
+  (let [admin-db "admin"
+        u        "***REMOVED***"
+        p        "***REMOVED***"
+        cred     (mcr/create u admin-db p)
+        host     "127.0.0.1"]
+    (mg/connect-with-credentials host cred)))
+
+(defn get-entries []
+  (let [conn (db-connection)
+        db   (mg/get-db conn "harvester")
+        coll "documents"]
+    (map #(select-keys % [:scenario :selection :session])
+         (mc/find-maps db "documents"))))
+
+(defn store-entry [e]
+  (let [conn (db-connection)
+        db   (mg/get-db conn "harvester")
+        coll "documents"]
+    (mc/insert db coll e)
+    e))
+
+(defroutes app-routes
+  (GET "/" []
+       {:status 200
+        :headers {"Content-Type" "application/json"}
+        :body {:message "Welcome to the BARN"}})
+
+  (GET "/entries" []
+       (let [entries (get-entries)]
+         (info entries)
+         {:status 200
+          :headers {"Content-Type" "application/json"}
+          :body entries}))
+
+  (POST "/entry" req
+        (let [body (cc/parse-string (slurp (:body req)) true)]
+          (info "\n-----\n")
+          (info (get-in req [:headers "origin"]))
+          (info "\n-----\n")
+          (if (or (= "https://harvester.mixing-senses.art" (get-in req [:headers "origin"]))
+                  (= "http://localhost:8080" (get-in req [:headers "origin"])))
+            (do
+              (store-entry body)
+              {:status 201
+               :headers {"Content-Type" "application/json"
+                         "Access-Control-Allow-Origin" "http://harvester.mixing-senses.art"
+                         "Access-Control-Allow-Headers" "Content-Type"}}
+              :body body)
+            {:status 403})))
+  
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-json-response app-routes))
